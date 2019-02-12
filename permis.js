1c59f507e5ab3a8752d1964f77806b99ca6aee0b
Devlop-er <michael.revellin@gmail.com>
2019-02-12 12:31:53 +0100

    Update permis.js

--- permis.js
+++ permis.js
@@ -1,6 +1,12 @@
-var age = prompt('Avant de démarrer la voiture j\'ai besoin de connaitre ton âge, Quel âge as-tu ?');
-if (Number(age) >= 18) {
-  alert('Tu as la majorité, je démarre le moteur !');
-} else if (Number(age) < 18) {
-  alert('Tu n\'as pas la majorité pour conduire...');
+function VerificationAge() {
+  var age = prompt('Avant de démarrer la voiture j\'ai besoin de connaitre ton âge, Quel âge as-tu ?');
+  if (Number(age) === 18) {
+    alert('Super tu as pile 18 ans, je démarre le moteur !');
+  } else if (Number(age) < 18) {
+    alert('Tu n\'as pas la majorité pour conduire...');
+  } else if (Number(age) > 18) {
+    alert('Tu est majeur, bonne conduite');
+  }
 }
+
+VerificationAge();
